8df1bcafd0579d0217eb5be10bf546a6f3ae09f8
ManasaDeshpande12 <119603404+ManasaDeshpande12@users.noreply.github.com>
2022-12-10 00:47:48 +0530

    Update Firstjava.java

--- Firstjava.java
+++ Firstjava.java
@@ -2,7 +2,7 @@ public class FirstJava
     {
       public static void main(String[] args)
        {
-         System.out.printlm("Hello! Welcome to Jenkins!Demonstrating freestyle job") ;
+         System.out.println("Hello! Welcome to Jenkins!Demonstrating freestyle job") ;
        }
     }
 
