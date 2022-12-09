948388f9ebcadaa5a909cc9617fb38ac27db337e
root <root@manasa.qxbuvqr4vszupengbim3ysvrge.rx.internal.cloudapp.net>
2022-12-09 18:51:43 +0000

    first commit

--- Firstjava.java
+++ Firstjava.java
@@ -1,7 +1,8 @@
- public class FirstJava
+public class FirstJava
     {
-      public sttaic void main(String[] args)
+      public static void main(String[] args)
        {
          System.out.pritnln("Hello! Welcome to Jenkins!") ;
        }
     }
+
